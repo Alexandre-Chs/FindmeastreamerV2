6d5d3b6acbd85474dfe534527e2139638de0e734
Alexandre-Chs <oliuuum@gmail.com>
2023-06-23 06:48:03 +0200

    modify color

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ import Main from "./Main";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between max-w-[1400px] mx-auto h-screen">
-      <h1 className="text-5xl bg-red-800">
+      <h1 className="text-5xl bg-red-500">
         Currently under construction, not everything is working properly yet!
         THANKS ♥.
       </h1>
