c7915b49631951a3b2bc9537a05e0d3799da3b0c
Alexandre-Chs <oliuuum@gmail.com>
2023-06-27 06:23:14 +0200

    test pw

--- src/app/api/getParticipants/route.ts
+++ src/app/api/getParticipants/route.ts
@@ -1,11 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "../../../../prisma/prisma";
 
-export async function GET() {
-  try {
-    const participants = await prisma.user.findMany();
-    return NextResponse.json({ participants: participants });
-  } catch (error) {
-    console.log(error);
+export async function GET(request: NextRequest) {
+  const expectedPassword = "123";
+  const cronPassword = request.headers.get("X-Cron-Password");
+  if (cronPassword !== expectedPassword) {
+    return NextResponse.json({ message: "acces interdit" }); // Mot de passe incorrect, retournez une réponse d'accès interdit
+  } else {
+    try {
+      const participants = await prisma.user.findMany();
+      return NextResponse.json({ participants: participants });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
