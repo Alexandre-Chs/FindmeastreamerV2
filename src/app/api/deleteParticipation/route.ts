37be42ecd038cbeda1a07fc66843c7a0b126b9ba
Alexandre-Chs <oliuuum@gmail.com>
2023-06-27 05:22:31 +0200

    fix error on delete route

--- src/app/api/deleteParticipation/route.ts
+++ src/app/api/deleteParticipation/route.ts
@@ -3,6 +3,9 @@ import prisma from "../../../../prisma/prisma";
 
 export async function DELETE(request: NextRequest) {
   const user = request.nextUrl.searchParams.get("user");
+  if (user === null) {
+    return NextResponse.json({ message: "user is null" });
+  }
   const now = new Date();
   const currentHour = now.getHours();
   const deleteResult = await prisma.user.deleteMany({
