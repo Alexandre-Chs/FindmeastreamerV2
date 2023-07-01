7cf9a2b5e1695d78bd37c7480da015860239aacd
Alexandre-Chs <oliuuum@gmail.com>
2023-07-01 07:26:59 +0200

    change getwinner

--- src/app/api/getWinner/route.ts
+++ src/app/api/getWinner/route.ts
@@ -4,21 +4,23 @@ import prisma from "../../../../prisma/prisma";
 export async function GET(request: Request) {
   const now = new Date();
   const currentHour = now.getHours();
+  const startOfCurrentDay = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate()
+  );
+  const endOfCurrentHour = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    currentHour
+  );
+
   const winner = await prisma.winner.findMany({
     where: {
       createdAt: {
-        gte: new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate(),
-          currentHour
-        ),
-        lt: new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate(),
-          currentHour + 1
-        ),
+        gte: startOfCurrentDay,
+        lt: endOfCurrentHour,
       },
     },
   });
