8e92977feefe274575b4454c70c0cd3b47443f1a
Alexandre-Chs <oliuuum@gmail.com>
2023-07-01 08:02:29 +0200

    add new changes

--- src/app/api/getWinner/route.ts
+++ src/app/api/getWinner/route.ts
@@ -4,26 +4,21 @@ import prisma from "../../../../prisma/prisma";
 export async function GET(request: Request) {
   const now = new Date();
   const currentHour = now.getHours();
-
-  // Déterminer la date et l'heure de début de l'heure précédente
-  const startOfPreviousHour = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    currentHour - 1
-  );
-  const endOfPreviousHour = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    currentHour
-  );
-
   const winner = await prisma.winner.findMany({
     where: {
       createdAt: {
-        gte: startOfPreviousHour,
-        lt: endOfPreviousHour,
+        gte: new Date(
+          now.getFullYear(),
+          now.getMonth(),
+          now.getDate(),
+          currentHour
+        ),
+        lt: new Date(
+          now.getFullYear(),
+          now.getMonth(),
+          now.getDate(),
+          currentHour + 1
+        ),
       },
     },
   });
