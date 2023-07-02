5f881434ce6170e79196aee3f4bce9518ced4ec7
Alexandre-Chs <oliuuum@gmail.com>
2023-07-02 10:06:57 +0200

    update getWinner route

--- src/app/api/getWinner/route.ts
+++ src/app/api/getWinner/route.ts
@@ -1,31 +1,33 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/prisma";
 
-export async function GET(request: Request) {
+export async function GET() {
   const now = new Date();
   const currentHour = now.getHours();
-  const winner = await prisma.winner.findMany({
-    where: {
-      createdAt: {
-        gte: new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate(),
-          currentHour
-        ),
-        lt: new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate(),
-          currentHour + 1
-        ),
+  try {
+    const winner = await prisma.winner.findMany({
+      where: {
+        createdAt: {
+          gte: new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            currentHour
+          ),
+          lt: new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            currentHour + 1
+          ),
+        },
       },
-    },
-  });
-  const response = NextResponse.json({ winner: winner });
+    });
 
-  // Ajouter l'en-tête de cache-control pour désactiver la mise en cache
-  response.headers.set("Cache-Control", "no-store");
-
-  return response;
+    const response = NextResponse.json({ winner: winner });
+    response.headers.set("Cache-Control", "no-cache");
+    return response;
+  } catch (error) {
+    console.log("Error on getWinner" + error);
+  }
 }
