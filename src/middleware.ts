256319b94377fd470b0bef73e85cb0b0628e129e
Alexandre-Chs <oliuuum@gmail.com>
2023-07-04 17:36:09 +0200

    setup middleware and secure some routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 
-// const passwordProtectedRoute = "/api/getCurrentParticipants";
-// const validPassword = "YOUR_PASSWORD";
-
-// export default createMiddleware({
-//   // A list of all locales that are supported
-//   locales: ["en", "fr", "es", "ko"],
-//   defaultLocale: "en",
-// });
-
 const passwordProtectedRoute = "/api/getCurrentParticipants";
 
 const i18nMiddleware = createMiddleware({
@@ -19,27 +10,21 @@ const i18nMiddleware = createMiddleware({
 });
 
 export function middleware(req: NextRequest, _res: NextResponse) {
-  const validPassword = "lol";
+  const validPassword = process.env.PASSWORD_QUERY_GET;
   console.log(req.nextUrl.pathname);
   if (req.nextUrl.pathname === passwordProtectedRoute) {
     const providedPassword = req.nextUrl.searchParams.get("password");
     console.log(validPassword);
 
     if (providedPassword?.toString() === validPassword) {
-      // Le mot de passe est valide, continuez le traitement de la requête
-      console.log("Le mot de passe est bon");
-      return i18nMiddleware(req);
+      return;
     } else {
-      // Le mot de passe est invalide, renvoyez une réponse avec un statut 403 (accès refusé)
       return new Response(null, { status: 403 });
     }
   }
-
-  // Route non protégée, continuez le traitement de la requête
   return i18nMiddleware(req);
 }
 
 export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ["/((?!api|_next|.*\\..*).*)", "/api/:path*"],
+  matcher: ["/((?!api|_next|.*\\..*).*)", "/api/getCurrentParticipants"],
 };
