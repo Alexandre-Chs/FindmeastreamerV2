63c73b7a04875534bc203692e5a2cc70c40a2bac
Alexandre <alexandre0chanas@gmail.com>
2023-07-05 11:57:43 +0200

    force dynamic

--- src/app/api/getCurrentParticipants/route.ts
+++ src/app/api/getCurrentParticipants/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/prisma";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
   const now = new Date();
