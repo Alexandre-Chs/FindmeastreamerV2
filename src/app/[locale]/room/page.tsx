8b186aec40b2a1e683c84a815fb1441c6434da32
Alexandre <alexandre0chanas@gmail.com>
2023-07-05 12:41:36 +0200

    merge local

--- src/app/[locale]/room/page.tsx
+++ src/app/[locale]/room/page.tsx
@@ -2,6 +2,7 @@ import WrapperNavRoom from "@/components/nav/WrapperNavRoom";
 import MainRoom from "./MainRoom";
 
 export default function Page() {
+  
   return (
     <div>
       <WrapperNavRoom page="room" />
