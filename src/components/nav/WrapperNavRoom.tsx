7693136aba078bde7df4d7edc9eb0354fb4b5b4b
Alexandre-Chs <oliuuum@gmail.com>
2023-07-07 06:56:56 +0200

    add support button, add new text

--- src/components/nav/WrapperNavRoom.tsx
+++ src/components/nav/WrapperNavRoom.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import Languages from "./Languages";
 import { typePageProps } from "@/types/pageType";
 import LotteryManager from "@/app/[locale]/room/LotteryManager";
+import { useTranslations } from "next-intl";
 
 const WrapperNavRoom = ({ page }: typePageProps) => {
+  const t = useTranslations("Home");
   return (
     <header className="flex flex-col items-center justify-between w-full gap-2 px-2 mt-5 border-b border-gray-700 sm:flex-row text-bold sm:px-4 pb-7">
       <a href="/">Findmeastreamer</a>
