1f647935bf6087478c13533442ac7d357ad9de01
AlexandreChs <alexandrechanas0@gmail.com>
2024-02-27 17:37:00 +0100

    update error message

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,11 +3,12 @@ import Footer from "./Footer";
 import Main from "./Main";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-between max-w-[1400px] mx-auto h-screen">
-      <WrapperNav page="home" />
-      <Main />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-between max-w-[1400px] mx-auto h-screen">
+            <span className='bg-red-700 text-4xl'>The site is currently experiencing problems with the Twitch API, thank you for your understanding, I am doing what is necessary to resolve the problem as quickly as possible. </span>
+            <WrapperNav page="home"/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
 }
